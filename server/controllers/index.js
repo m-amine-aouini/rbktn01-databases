d6b782cf285d9877d6d0f0054693378e5b9889c4
aminaouini <aminaouini565@gmail.com>
2020-03-28 14:10:29 +0100

    done with modules and controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,54 +1,26 @@
 var models = require('../models');
-var { mysql } = require('./../db/index.js');
+var { messages, users } = require('./../models/index.js');
 
 module.exports = {
   messages: {
     get: function (req, res) {
 
-      mysql.query('SELECT * FROM messages', (err, results) => {
-        if (err) {
-          throw err;
-        }
-        res.send({ results });
-      });
+      messages.get(req, res);
 
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      let { message, username, roomname } = req.body;
-      mysql.query('INSERT INTO messages SET ?', {
-        text: message,
-        username,
-        roomname
-      }, (err, res) => {
-        if (err) {
-          throw err;
 
-        }
-        console.log('message inserted');
-      });
-      res.end();
-
+      messages.post(req, res);
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-      mysql.query('SELECT * FROM users', (err, results) => {
-        if (err) {
-          throw err;
-        }
-        res.send({ results });
-      });
+      users.get(req, res);
     },
     post: function (req, res) {
-      mysql.query('INSERT INTO users SET ?', req.body, (err, res) => {
-        if (err) {
-          throw err;
-        }
-        console.log('user inserted');
-      });
-      res.end();
+      users.post(req, res);
     }
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,52 @@
 var db = require('../db');
+var { mysql } = require('./../db/index.js');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) {
+      mysql.query('SELECT * FROM messages', (err, results) => {
+        if (err) {
+          throw err;
+        }
+        res.send({ results });
+      });
+    }, // a function which produces all the messages
+    post: function (req, res) {
+      let { message, username, roomname } = req.body;
+      mysql.query('INSERT INTO messages SET ?', {
+        text: message,
+        username,
+        roomname
+      }, (err, res) => {
+        if (err) {
+          throw err;
+
+        }
+        console.log('message inserted');
+      });
+      res.end();
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (req, res) {
+      mysql.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+          throw err;
+        }
+        res.send({ results });
+      });
+    },
+    post: function (req, res) {
+      mysql.query('INSERT INTO users SET ?', req.body, (err, res) => {
+        if (err) {
+          throw err;
+        }
+        console.log('user inserted');
+      });
+      res.end();
+    }
   }
 };
 
